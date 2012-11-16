5b8a7025752df47a71d59c92ef74c7b331dc9cb5
sunjunjian <samsun_wsu@yahoo.com>
2012-11-16 14:23:21 -0800

    modified .py variables names

--- Python/matrixProduct.py
+++ Python/matrixProduct.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env
+# -*- coding:utf-8 -*-
+
 #	matrixProduct.py  --- uses 2D lists to create matrix objects demonstrating
 #		how matrix multiplicaion works.
-
 #	1.a 	Multiplication can be done by the naive algorithm - ijk iterations.
 #	1.b 	With the beauty of python language, the triple loops can be replaced by a single line.
 #	2.   	A better appraoch is to use numpy/scipy packages.
@@ -10,13 +12,13 @@ import numpy as np
 
 
 
-def loadFromFile(fileName):
+def LoadFromFile(filename):
 	matrix = []
 
 	try:
-		f = open(fileName, 'r')
+		f = open(filename, 'r')
 	except FileNotFoundError:
-		print	'Oops! No such file or directory:', fileName
+		print	'Oops! No such file or directory:', filename
 		return	matrix
 	else:
 		lines = f.readlines()
@@ -29,40 +31,40 @@ def loadFromFile(fileName):
 
 
 
-def naiveAlg(matrixA, matrixB):
+def NaiveAlg(matrix_a, matrix_b):
 	try:
-		if len(matrixA[0]) != len(matrixB):
+		if len(matrix_a[0]) != len(matrix_b):
 			raise	SizeMismatch
 	except SizeMismatch:
 		print	'Oops! Unable to multiply the matrices due to size mismatch.'
-		matrixC = []
-		return	matrixC
+		matrix_c = []
+		return	matrix_c
 
 
-	rows = len(matrixA)
-	columns = len(matrixB[0])
+	rows = len(matrix_a)
+	columns = len(matrix_b[0])
 
-	matrixC = [[0 for col in range(columns)] for row in range(rows)]
+	matrix_c = [[0 for col in range(columns)] for row in range(rows)]
 
 	for i in range(rows):
 		for j in range(columns):
-			for k in range(len(matrixB)):
-				matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
+			for k in range(len(matrix_b)):
+				matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
 
-	return	matrixC
+	return	matrix_c
 
 
-def oneLineAlg(matrixA, matrixB):
+def OneLineAlg(matrix_a, matrix_b):
 	try:
-		if len(matrixA[0]) != len(matrixB):
+		if len(matrix_a[0]) != len(matrix_b):
 			raise	SizeMismatch
 	except SizeMismatch:
 		print	'Oops! Unable to multiply the matrices due to size mismatch.'
-		matrixC = []
-		return	matrixC
+		matrix_c = []
+		return	matrix_c
 	else:
 		# use zip to iterate over two lists in parallel
-		return	[ [sum(a * b for a, b in zip(a, b)) for b in zip(*matrixB)] for a in matrixA ]
+		return	[ [sum(a * b for a, b in zip(a, b)) for b in zip(*matrix_b)] for a in matrix_a ]
 
 
 
@@ -72,19 +74,21 @@ if __name__ == "__main__":
 	file1 = '../inputFiles/a_8x8.in'
 	file2 = '../inputFiles/b_8x8.in'
 
-	A = loadFromFile(file1)
-	B = loadFromFile(file2)
+	a = LoadFromFile(file1)
+	b = LoadFromFile(file2)
 	
-	#C = naiveAlg(A, B)
+	# naive algorithm 
+	# c = NaiveAlg(a, b)
 
-	C = oneLineAlg(A, B)
+	c = OneLineAlg(a, b)
 
-	#A = np.matrix(A)
-	#B = np.matrix(B)
-	#C = A * B
+	# numpy library
+	# a = np.matrix(a)
+	# b = np.matrix(a)
+	# c = a * b
 
-	for i in range(len(C)):
-		print C[i]
+	for i in range(len(c)):
+		print c[i]
 
 
 
